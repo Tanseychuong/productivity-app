b79e2cbc3cc8883ea8789b1a65ff3f06274401e7
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-09-20 21:06:13 +0000

    Remove user table schema from database schema_1.sql

--- Backend/database/schema_1.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS user(
-    varchar(50) primary key,
-    varchar(30) first_name not null,
-    varchar(30) middle_name, 
-    varchar(30) last_name not null,
-    image profile_picture,
-    text(500) bio_description
-);
