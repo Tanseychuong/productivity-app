0678d1d46efec595f91479eb5c5a7a9aec9d94c6
Tanseychuong <nyangchuongtiutiu@gmail.com>
2026-09-21 00:24:34 +0000

    Add Contacts app with models and forms for managing contact information

---
+++ apps/contacts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}".strip()
